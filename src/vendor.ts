9d369c075ac76e237551d2d0935b08f5eaa8f1c0
Nathan Walker <walkerrunpdx@gmail.com>
2016-10-12 00:26:35 -0700

    fix: dev build server
    updated packages and fixed dev server
    
    closes https://github.com/NathanWalker/ng2-image-lazy-load/issues/16

--- src/vendor.ts
+++ src/vendor.ts
@@ -7,6 +7,7 @@ import '@angular/http';
 import '@angular/router';
 
 import 'rxjs';
+import '@angularclass/hmr';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
